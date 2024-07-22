1487f3cfd7e9006ee4e7d8144d5b5ec1a16f9318
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 15:59:54 +0530

    Create Sandglass.java

---
+++ Sandglass.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int i,j;
+        for(i=n;i>1;i--)
+        {
+            for(j=i;j<=n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=2*i-1;j++)
+            {  
+                if(j==2*i-1 || j==1||i== n)
+                {
+                System.out.print("*") ;
+                    
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for(i=1;i<=n;i++)
+        {
+            for(j=i;j<=n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=2*i-1;j++)
+            {  
+                if(j==2*i-1 || j==1||i== n)
+                {
+                System.out.print("*") ;
+                    
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();}
+    }
+}
+OUTPUT:
+ *********
+  *     *
+   *   *
+    * *
+     *
+    * *
+   *   *
+  *     *
+ *********
