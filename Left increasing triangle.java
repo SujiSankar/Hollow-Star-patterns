9eea6995694513c784c57a0cb87cb2d820f75b16
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 14:49:13 +0530

    Create Left increasing triangle.java

---
+++ Left increasing triangle.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=i;j<=n;j++)
+            {
+                 System.out.print(" ");
+            }
+           
+            for(j=1;j<=i;j++){
+                if(j==i||i==n||j==1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        }
+    }
+OUTPUT:
+     *
+    **
+   * *
+  *  *
+ *****
