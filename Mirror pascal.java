0717eb14365ed806c3ed290035aff48ca35a9a79
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 15:55:55 +0530

    Create Mirror pascal.java

---
+++ Mirror pascal.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int i,j;
+        for(i=1;i<n;i++){
+            for(j=i;j<=n;j++)
+            {
+                System.out.print("  ");
+            }
+           
+            for(j=1;j<=i;j++){
+                if(j==i||j==1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for(i=n;i>=1;i--){
+            for(j=i;j<=n;j++)
+            {
+                System.out.print("  ");
+            }
+           
+           
+            for(j=1;j<=i;j++){
+                if(j==i||j==1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        }
+    }
+OUTPUT:
+          * 
+        * * 
+      *   * 
+    *     * 
+  *       * 
+    *     * 
+      *   * 
+        * * 
+          * 
