e794d39f271f2b66deb3ea0c815ce5509787bf6e
Sujithra S <padmapriya13451@gmail.com>
2024-07-22 14:47:16 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,27 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(i==1||i==n||j==1||j==n){
+                System.out.print("*");
+            }
+            else
+        {
+            System.out.print(" ");
+        }
+        }
+        
+        
+        System.out.println();
+        }
+    }
+}
+OUTPUT:
+*****
+*   *
+*   *
+*   *
+*****
